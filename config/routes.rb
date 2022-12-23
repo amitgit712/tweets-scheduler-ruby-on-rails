99639989e1558c81186d6fefe44edaad1ef22635
ankit <ankitgit21@gmail.com>
2022-12-23 21:47:56 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   get "about", to: "about#index" #, as: :about #optional
+  
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
   root to: "main#index"
 end
